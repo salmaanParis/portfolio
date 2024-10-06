a588f7c380ffff8b2245c624919b7b91126ac406
salmaanParis <youtonez@gmail.com>
2024-10-07 04:00:07 +0530

    first commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,68 @@
+"use client";
+import React from 'react'
+import Image from 'next/image'
+import cursor from "../assets/icon1.png"
+import lightning from "../assets/icon2.png"
+import { motion } from 'framer-motion';
+import profilepic from "../assets/profilepic.png"
+
+const Hero = () => {
+  return (
+    <div className="py-24 relative overflow-clip bg-[linear-gradient(to_bottom,#000,#2B1942_35%,#8F5C55_60%,#DBAF6E_80%)] ">
+        
+        <div className='absolute rounded-[50%] w-[3000px] h-[1300px] top-[550px] left-[50%] -translate-x-1/2 bg-[radial-gradient(closest-side,#000_80%,#2B1942)]'></div>
+
+        <div className='relative'>
+            <div className='text-8xl font-bold text-center'>
+                <h1 className='text-[#98b4CE]'>Hi, I am</h1>
+                <h1 className='text-[#E48A57]'>Salman Paris</h1>
+            </div>
+       
+
+              <motion.div
+            className="hidden md:block absolute left-[280px] top-[170px]"drag>
+                
+                <Image
+                src={cursor}
+                height={"170"}
+                width={"170"}
+                alt="cursor"
+                className=''
+                draggable="false"
+                    
+                />
+
+                </motion.div>
+
+                <motion.div
+            className="hidden md:block absolute right-[220px] top-[20px]"drag>
+                
+                <Image
+                src={lightning}
+                height={"120"}
+                width={"120"}
+                alt="message"
+                className=''
+                draggable="false"
+                    
+                />
+
+                </motion.div>
+           
+
+            <p className='text-center text-xl max-w-[500px] mx-auto mt-8 text-white/80'>
+                    I am a full stack developer
+            </p>
+
+            <Image
+                src={profilepic}
+                alt="profile picture"
+                className='h-auto w-auto mx-auto'
+                />
+             </div>
+        
+        </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Skills.tsx
@@ -0,0 +1,54 @@
+"use client"
+import React from 'react'
+
+import { SiTailwindcss, SiTypescript, SiNodedotjs, SiMongodb, SiNextdotjs, SiExpress } from 'react-icons/si';
+import { FaHtml5, FaCss3Alt, FaReact, FaJsSquare, FaGit, FaDocker } from 'react-icons/fa';
+import { SiRedux, SiFirebase, SiVercel, SiNetlify, SiFigma, SiFramer } from 'react-icons/si';
+
+
+
+const skillIcons = [
+    { icon: <FaHtml5 size={140}/>, label: "HTML" },
+    { icon: <FaCss3Alt size={140}/>, label: "CSS" },
+    { icon: <FaJsSquare size={140}/>, label: "JavaScript" },
+    { icon: <FaReact size={140}/>, label: "React" },
+    { icon: <SiRedux size={140}/>, label: "Redux" },
+    { icon: <SiTypescript size={140}/>, label: "TypeScript" },
+    { icon: <SiTailwindcss size={140}/>, label: "Tailwind CSS" },
+    { icon: <SiNodedotjs size={140}/>, label: "Node.js" },
+    { icon: <SiExpress size={140}/>, label: "Express" },
+    { icon: <SiMongodb size={140}/>, label: "MongoDB" },
+    { icon: <SiNextdotjs size={140}/>, label: "Next.js" },
+    { icon: <FaGit size={140}/>, label: "Git" },
+    { icon: <FaDocker size={140}/>, label: "Docker" },
+    { icon: <SiFirebase size={140}/>, label: "Firebase" },
+    // { icon: <SiVercel size={140}/>, label: "Vercel" },
+    // { icon: <SiNetlify size={140}/>, label: "Netlify" },
+    { icon: <SiFigma size={140}/>, label: "Figma" },
+    { icon: <SiFramer size={140}/>, label: "Framer Motion" }
+]
+
+const Skills = () => {
+  return (
+    <div className='bg-[linear-gradient(to_top,#000,#381a5f_80%)] py-32'>
+        <div className='text-white w-[400px] md:min-w-[950px] mx-auto p-8 text-center'>
+            <h2 className='text-6xl font-bold mb-4'>What I Do</h2>
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-6'>
+                {skillIcons.map((skill,index) => (
+                    <div key={index} className='h-[160px] w-[160px] md:h-[220] md:w-[220px] flex flex-col justify-between items-center bg-white/10 p-4 rounded-xl'>
+                        {skill.icon}
+                        <p className='mt-2'>{skill.label}</p>
+                        {}
+
+                    </div>
+
+                ))}
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Skills
